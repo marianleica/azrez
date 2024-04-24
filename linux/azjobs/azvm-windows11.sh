b728a983b6b96225f9b31d03031e09e00056ff8d
Marian Leica <maleica@microsoft.com>
2024-04-24 09:31:23 +0300

    Add files via upload

---
+++ linux/azjobs/azvm-windows11.sh
@@ -0,0 +1,64 @@
+# Setting variables
+suffix=$((10000 + RANDOM % 99999))
+rg="azresourcelauncher"
+location="uksouth"
+vmName="azvm-windows11-${suffix}"
+image='MicrosoftWindowsDesktop:windows-11:win11-23h2-pro:22631.2428.231023'
+# az vm image list -f windows-11 -o table --all 
+
+# Generating a random string to use as password
+#!/bin/bash
+array=()
+for i in {a..z} {A..Z} {0..9}; 
+   do
+   array[$RANDOM]=$i
+done
+password=$(printf %s ${array[@]::30} $'\n')
+
+echo "Creating virtual machine $vmName in resource group $rg in location $location"
+sleep 1
+echo ""
+echo "The resource group:"
+# Create RG
+az group create -n $rg -l $location
+
+# Create NSG
+#az network nsg create --name NSG4VM --resource-group $rg
+#az network nsg rule create \
+#--name inboundrule4vm \
+#--nsg-name NSG4VM \
+#--priority 200 \
+#--resource-group $rg \
+#--access Allow \
+#--destination-port-ranges 3383 \
+#--direction Inbound \
+#--protocol Tcp
+
+sleep 1
+echo ""
+echo "The virtual machine $vmName:"
+# Create Windows 11
+az vm create -g $rg -n $vmName \
+--image $image \
+--admin-user "azrez" --admin-password $password \
+--public-ip-sku Standard \
+--nsg NSG4VM \
+--nsg-rule RDP
+
+sleep 2
+# This is the public IP address
+vmip=$(az vm list-ip-addresses -g $rg -n $vmName \
+--query "[].virtualMachine.network.publicIpAddresses[0].ipAddress" --output tsv)
+
+sleep 1
+echo ""
+echo "The public IP address allocated to VM $vmName is $vmip"
+echo "The admin user name is: azrez"
+echo "The unique password is: $password"
+
+#pwsh
+#$rg='myVM'
+#$location='northeurope'
+#$vmName='winclient1'
+#Get-AzRemoteDesktopFile -ResourceGroupName $rg -Name $vmName -Launch
+
