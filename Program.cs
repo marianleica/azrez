0ffc936dd927f2dfd46179c52ad8fb8b8691c352
marianleica <maleica@microsoft.com>
2024-05-24 13:37:04 +0300

    Sync

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// AZRESOURCELAUNCHER v1.0 //
+
+// system session
+using System;
+using System.Diagnostics;
+
+// Storing
+
+string initialSetup = "";
+string flow = "";
+string menuSelection = "";
+
+do
+{
+
+} while
