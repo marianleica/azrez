da5f6a15cef55c33e73771dfc868b49287675610
marianleica <maleica@microsoft.com>
2024-06-07 11:15:31 +0300

    Code sync. Testing {3, 1} succeeded

--- azjobs/azaks-public-kubenet-lb.sh
+++ azjobs/azaks-public-kubenet-lb.sh
@@ -4,6 +4,7 @@ RG="azresourcelauncher" # Name of resource group for the AKS cluster
 location="uksouth" # Name of the location 
 AKS="aks-kubenetlb-${namesuffix}" # Name of the AKS cluster
 
+echo ""
 echo "Creating AKS cluster $AKS in resource group $RG"
 # Create new Resource Group
 az group create -g $RG -l $location
@@ -16,6 +17,7 @@ sleep 5
 # aksextension=$(az aks show --resource-group $aksClusterGroupName --name $aksName --query id --output tsv)
 # az resource wait --ids $aksextension --custom "properties.provisioningState!='Creating'"
 
+echo ""
 # Get the AKS infrastructure resource group name
 infra_rg=$(az aks show --resource-group $RG --name $AKS --output tsv --query nodeResourceGroup)
 echo "The infrastructure resource group is $infra_rg"
@@ -25,6 +27,7 @@ echo "The infrastructure resource group is $infra_rg"
 # Install kubectl locally:
 # az aks install-cli
 
+echo ""
 sleep 1
 echo "Configuring "kubectl" to connect to the Kubernetes cluster"
 # echo "If you want to connect to the cluster to run commands, run the following:"
