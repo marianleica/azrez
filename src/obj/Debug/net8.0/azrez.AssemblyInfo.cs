026caf49058fec01ebd2b37d0b93c74c245ab05c
marianleica <maleica@microsoft.com>
2024-06-29 08:15:32 +0300

    Code sync

--- src/obj/Debug/net8.0/azrez.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/azrez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azrez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a493c167ba2be535b86aa4c85b7c6f8541818b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc5e1d9541d4fdd20a6c827e666987987937913")]
 [assembly: System.Reflection.AssemblyProductAttribute("azrez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azrez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
