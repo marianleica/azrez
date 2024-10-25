0ba4179bfea143e0f9edb663ef74143d82a97921
Marian Leica <maleica@microsoft.com>
2024-10-25 20:22:47 +0300

    Update aksArcAppServiceExt.ps1

--- pwshjobs/aksArcAppServiceExt.ps1
+++ pwshjobs/aksArcAppServiceExt.ps1
@@ -35,8 +35,9 @@ Write-Output ""
 Write-Output "Onboarding cluster {$AKS} to Azure Arc-enabled Kubernetes"
 # Onboarding the cluster to Azure Arc-enabled Kubernetes
 $ARC="arc-aks-${suffix}" # Name of the ARC cluster
-az extension add --name connectedk8s
+az extension add --upgrade --name connectedk8s
 az connectedk8s connect --resource-Group $RG --name $ARC
+az provider register --namespace Microsoft.Web --wait
 
 Start-Sleep -Seconds 5
 Write-Output ""
@@ -44,3 +45,45 @@ Write-Output "The azure-arc namespace status:"
 # Showcase the azure-arc namespace
 az aks command invoke --resource-group $RG --name $AKS --command "kubectl get all -n azure-arc"
 Write-Output ""
+
+# Setting variables:
+$extensionName="appservice-ext" # Name of the App Service extension
+$namespace="appservice-ns" # Namespace in your cluster to install the extension and provision resources
+$kubeEnvironmentName="kube-environment" # Name of the App Service Kubernetes environment resource
+
+# Install the appservice extension:
+az k8s-extension create --resource-group $RG --name $extensionName --cluster-type connectedClusters --cluster-name $ARC --extension-type 'Microsoft.Web.Appservice' --release-train stable --auto-upgrade-minor-version true --scope cluster --release-namespace $namespace --configuration-settings "Microsoft.CustomLocation.ServiceAccount=default" --configuration-settings "appsNamespace=${namespace}" --configuration-settings "clusterName=${kubeEnvironmentName}" --configuration-settings "keda.enabled=true" --configuration-settings "buildService.storageClassName=default" --configuration-settings "buildService.storageAccessMode=ReadWriteOnce" --configuration-settings "customConfigMap=${namespace}/kube-environment-config" --configuration-settings "envoy.annotations.service.beta.kubernetes.io/azure-load-balancer-resource-group=${aksClusterGroupName}"
+
+Start-Sleep -Seconds 5
+Write-Output ""
+# Save the id of the appservice extension for the next step:
+$extensionId=$(az k8s-extension show --cluster-type connectedClusters --cluster-name $clusterName --resource-group $groupName --name $extensionName --query id --output tsv)
+
+# Wait for the fully install before proceeding:
+az resource wait --ids $extensionId --custom "properties.installState!='Pending'" --api-version "2020-07-01-preview"
+Start-Sleep -Seconds 5
+Write-Output ""
+
+# Step 1.3 - Create a custom location
+# Set the required variables:
+$customLocationName="arc-location" # Name of the custom location
+$connectedClusterId=$(az connectedk8s show --resource-group $RG --name $clusterName --query id --output tsv)
+
+# Create the custom location:
+az customlocation create --resource-group $RG --name $customLocationName --host-resource-id $connectedClusterId --namespace $namespace --cluster-extension-ids $extensionId
+
+Start-Sleep -Seconds 5
+Write-Output ""
+# Validate the custom location creation:
+az customlocation show --resource-group $RG --name $customLocationName
+
+# Save the custom location ID for the next step:
+$customLocationId=$(az customlocation show --resource-group $RG --name $customLocationName --query id --output tsv)
+
+# Step 1.4 - Create the App Service Kubernetes Environment
+az appservice kube create --resource-group $RG --name $kubeEnvironmentName --custom-location $customLocationId
+
+Start-Sleep -Seconds 5
+Write-Output ""
+# Validate that the App Service Kubernetes Environment has been successfully created:
+az appservice kube show --resource-group $RG --name $kubeEnvironmentName
