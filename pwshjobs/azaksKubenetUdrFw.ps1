5f1f35b8a0d26ccd5a7e34ee8f5d9954b9e2490c
Marian Leica <maleica@microsoft.com>
2024-10-26 14:28:13 +0300

    Update azaksKubenetUdrFw.ps1

--- pwshjobs/azaksKubenetUdrFw.ps1
+++ pwshjobs/azaksKubenetUdrFw.ps1
@@ -148,4 +148,6 @@ Start-Sleep -Seconds 2
 # Adding NAT rule
 az network firewall nat-rule create --collection-name exampleset --destination-addresses $FWPUBLIC_IP --destination-ports 80 --firewall-name $FWNAME --name inboundrule --protocols Any --resource-group $RG --source-addresses '*' --translated-port 80 --action Dnat --priority 100 --translated-address $SERVICE_IP
 
+Write-Output ""
+Read-Host "Press any key to continue..."
 #########################################
