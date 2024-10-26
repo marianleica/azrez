905283b9bdde861bb1b0dba008f29602312e3ac7
Marian Leica <maleica@microsoft.com>
2024-10-26 13:02:54 +0300

    Update aksArcAppServiceExt.ps1

--- pwshjobs/aksArcAppServiceExt.ps1
+++ pwshjobs/aksArcAppServiceExt.ps1
@@ -32,7 +32,7 @@ az aks get-credentials --resource-group $RG --name $AKS --admin --overwrite-exis
 
 Start-Sleep -Seconds 1
 Write-Output ""
-Write-Output "Onboarding cluster {$AKS} to Azure Arc-enabled Kubernetes"
+Write-Output "Onboarding cluster ${AKS} to Azure Arc-enabled Kubernetes"
 # Onboarding the cluster to Azure Arc-enabled Kubernetes
 $ARC="arc-aks-${suffix}" # Name of the ARC cluster
 az extension add --upgrade --name connectedk8s
