95330abbd9906df24a0b9adb3de80d445c8706a0
Marian Leica <maleica@microsoft.com>
2024-10-26 12:11:37 +0300

    Update azvm-windowsserver2022.ps1

--- pwshjobs/azvm-windowsserver2022.ps1
+++ pwshjobs/azvm-windowsserver2022.ps1
@@ -4,46 +4,46 @@ Start-Sleep -Seconds 1
 # Setting variables
 $suffix=$(Get-Random -Minimum 1000 -Maximum 9999)
 #suffix=$((10000 + RANDOM % 99999))
-$rg="azrez"
+$RG="azrez"
 $location="uksouth"
-$vmName="azvm-win22-${suffix}"
+$VM="azvm-win22-${suffix}"
 $image='MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest'
-$publicIp="winserver22IP-${suffix}"
+$publicIp="winsrv22IP-${suffix}"
 
 # Generating a random string to use as password
 $UserName = "azrez"
 $randompass = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 30 | ForEach-Object {[char]$_})
 #Read more: https://www.sharepointdiary.com/2020/04/powershell-generate-random-password.html#ixzz8XiwccFos
-$Password = ConvertTo-SecureString $randompass -AsPlainText -Force
-$psCred = New-Object System.Management.Automation.PSCredential($UserName, $Password)
+#$Password = ConvertTo-SecureString $randompass -AsPlainText -Force
+#$psCred = New-Object System.Management.Automation.PSCredential($UserName, $Password)
 
-Write-Output "Creating virtual machine $vmName in resource group $rg in location $location"
+Write-Output "Creating virtual machine $VM in resource group $RG in location $location"
 Start-Sleep -Seconds 1
 Write-Output ""
 
 Write-Output "The Resource Group:"
 # Create RG
-az group create -n $rg -l $location
+az group create -n $RG -l $location
 Start-Sleep -Seconds 1
 Write-Output ""
-Write-Output "The virtual machine $vmName:"
+Write-Output "The virtual machine $VM:"
 
 # Create Windows Server 2022
 # New-AzVm -ResourceGroupName $rg -Name $vmName -Location $location -Image $image -VirtualNetworkName "myVnet-${suffix}" -SubnetName "vmsubnet" -SecurityGroupName "vmNSG" -PublicIpAddressName $publicIp -OpenPorts 80,3389
-az vm create -g $rg -n $vmName --image $image --admin-user "azrez" --admin-password $password --public-ip-sku Standard --nsg NSG4VM --nsg-rule RDP
+az vm create -g $RG -n $VM --image $image --admin-user "azrez" --admin-password $randompass --public-ip-sku Standard --nsg NSG4VM --nsg-rule RDP
 
 Start-Sleep -Seconds 2
 # This is the public IP address
-$vmip=$(az vm list-ip-addresses -g $rg -n $vmName --query "[].virtualMachine.network.publicIpAddresses[0].ipAddress" --output tsv)
+$vmip=$(az vm list-ip-addresses -g $RG -n $VM --query "[].virtualMachine.network.publicIpAddresses[0].ipAddress" --output tsv)
 #$vmip=$(Get-AzPublicIpAddress -Name $publicIp -ResourceGroupName $rg)
 
 Start-Sleep -Seconds 1
 Write-Output ""
-Write-Output "The public IP address allocated to VM {$vmName} is {$vmip}"
+Write-Output "The public IP address allocated to VM ${VM} is ${vmip}"
 Write-Output "The admin user name is: azrez"
-Write-Output "The unique password is: {$password}"
+Write-Output "The unique password is: ${randompass}"
 
-Start-Sleep -Seconds 20
+Read-Host "Press any key to continue..."
 #pwsh
 #$rg='myVM'
 #$location='northeurope'
