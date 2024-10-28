99a13b75efa38efc786a6bb643d42f60cdcd7329
Marian Leica <maleica@microsoft.com>
2024-10-28 20:23:56 +0200

    Update azsetup.ps1

--- pwshjobs/azsetup.ps1
+++ pwshjobs/azsetup.ps1
@@ -10,22 +10,9 @@ Write-Output "Running initial setup for Windows development environment"
 
 Start-Sleep -Seconds 1
 
-Write-Output "Install winget, if not present"
-# Install winget
-$progressPreference = 'silentlyContinue'
-Write-Information "Downloading WinGet and its dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-
-Start-Sleep -Seconds 1
-
 Write-Output "Install Az CLI"
 # Install az cli on windows
-winget install -e --id Microsoft.AzureCLI
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
 
 Start-Sleep -Seconds 1
 
