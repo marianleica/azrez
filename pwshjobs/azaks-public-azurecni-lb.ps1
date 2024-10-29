42e1085ba0fc9bc4baa21bad6b0c6385784dabe1
Marian Leica <maleica@microsoft.com>
2024-10-29 15:56:15 +0200

    Update azaks-public-azurecni-lb.ps1

--- pwshjobs/azaks-public-azurecni-lb.ps1
+++ pwshjobs/azaks-public-azurecni-lb.ps1
@@ -43,6 +43,6 @@ Write-Output "Configuring kubectl to connect to the Kubernetes cluster"
 # az aks get-credentials --resource-group $RG --name $AKS --admin --overwrite-existing
 az aks get-credentials --resource-group $RG --name $AKS --admin --overwrite-existing
 
-Write-Output ""
 Write-Output "You should be able to run kubectl commands to your cluster now"
+Write-Output ""
 Read-Host "Press any key to continue..."
