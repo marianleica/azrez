08e3ebb4acfd43a06641a10ade8a40abf08c3fee
Marian Leica <maleica@microsoft.com>
2024-10-29 09:50:28 +0200

    Update azaks-public-azurecni-lb.ps1

--- pwshjobs/azaks-public-azurecni-lb.ps1
+++ pwshjobs/azaks-public-azurecni-lb.ps1
@@ -7,14 +7,11 @@ $AKS="aks-azurecni-${suffix}" # Name of the AKS cluster
 Write-Output "Creating AKS cluster ${AKS} in resource group ${RG}"
 # Create new Resource Group
 Write-Output "The resource group: "
-# az group create -g $RG -l $location
 az group create -n $RG -l $location
 Write-Output ""
 
 # Create virtual network and subnets
 Write-Output "The BYO VNET: "
-# az network vnet create --resource-group $RG --name aksVnet --address-prefixes 10.0.0.0/8 --subnet-name aks_subnet --subnet-prefix 10.240.0.0/16
-
 az network vnet create --resource-group $RG --name aksVnet --address-prefixes 10.0.0.0/8 --subnet-name aks_subnet --subnet-prefix 10.240.0.0/16
 
 Write-Output ""
@@ -29,22 +26,15 @@ Write-Output ""
 Start-Sleep 2
 
 Write-Output "The AKS cluster: "
-# az aks create --resource-group $RG --name $AKS --node-count 1 --network-plugin azure --vnet-subnet-id $subnetId --enable-aad --generate-ssh-keys
 az aks create --resource-group $RG --name $AKS --node-count 1 --network-plugin azure --vnet-subnet-id $subnetId --enable-aad --generate-ssh-keys
-
 Start-Sleep -Seconds 5
-# Wait for the AKS cluster creation to be in Running state
-# aksextension=$(az aks show --resource-group $aksClusterGroupName --name $aksName --query id --output tsv)
-# az resource wait --ids $aksextension --custom "properties.provisioningState!='Creating'"
 
 # Get the AKS infrastructure resource group name
 $infra_rg=$(az aks show --resource-group $RG --name $AKS --output tsv --query nodeResourceGroup)
 Write-Output "The infrastructure resource group is ${infra_rg}"
 
-# sleep 1
-# echo "Let's see if you have 'kubectl' installed locally. Please ignore any errors."
-# Install kubectl locally:
-# az aks install-cli
+Write-Output ""
+Write-Output "Install kubectl locally, if needed: az aks install-cli"
 
 Write-Output ""
 Start-Sleep -Seconds 1
