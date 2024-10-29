b8a20043f14840b20a0e56ec1ce1e98db90cdd10
Marian Leica <maleica@microsoft.com>
2024-10-29 15:57:44 +0200

    Update azaks-windowsnp.ps1

--- pwshjobs/azaks-windowsnp.ps1
+++ pwshjobs/azaks-windowsnp.ps1
@@ -49,7 +49,7 @@ Write-Output "Configuring kubectl to connect to the Kubernetes cluster"
 # echo "If you want to connect to the cluster to run commands, run the following:"
 # az aks get-credentials --resource-group $RG --name $AKS --admin --overwrite-existing
 az aks get-credentials --resource-group $RG --name $AKS --admin --overwrite-existing
+Write-Output "You should be able to run kubectl commands to your cluster now"
 
 Write-Output ""
-Write-Output "You should be able to run kubectl commands to your cluster now"
 Read-Host "Press any key to continue..."
