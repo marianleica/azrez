1f0ddee5351c15e4d971b6787f1a0c4dea1c153d
Marian Leica <maleica@microsoft.com>
2024-10-29 14:35:17 +0200

    Update azvm-windows11.ps1

--- pwshjobs/azvm-windows11.ps1
+++ pwshjobs/azvm-windows11.ps1
@@ -32,7 +32,7 @@ Write-Output "The virtual machine ${VM}:"
 
 # Create Windows 11
 # New-AzVm -ResourceGroupName $rg -Name $vmName -Location $location -Image $image -VirtualNetworkName "myVnet-${suffix}" -SubnetName "vmsubnet" -SecurityGroupName "vmNSG" -PublicIpAddressName $publicIp -OpenPorts 80,3389
-az vm create -g $RG -n $VM --image $image --admin-user $userName --admin-password $randompass --public-ip-sku Standard --nsg NSG4VM --nsg-rule RDP
+az vm create -g $RG -n $VM --image $image --admin-user $userName --admin-password $randompass --public-ip-sku Standard --nsg NSG4VM --nsg-rule RDP --size Standard_D2s_v3
 
 Start-Sleep -Seconds 2
 # This is the public IP address
