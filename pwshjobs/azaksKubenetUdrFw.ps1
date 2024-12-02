e9badbb405af5ac4de872dd201d60254c94fbfd1
Marian Leica <maleica@microsoft.com>
2024-12-02 16:49:23 +0200

    Update azaksKubenetUdrFw.ps1

--- pwshjobs/azaksKubenetUdrFw.ps1
+++ pwshjobs/azaksKubenetUdrFw.ps1
@@ -4,6 +4,8 @@ Write-Output "Grab a coffee, this can take several minutes to complete.."
 
 $timestamp = $(Get-Date -Format "yyyy/MM/dd-HH:mm UTCK")
 $scenario = "azaksKubenetUdrFw"
+$timestamp = $(Get-Date -Format "yyyy/MM/dd-HH:mm UTCK")
+$scenario = "azaksKubenetUdrFw"
 $PREFIX="aks-azurecni-udr"
 $SUFFIX=$(Get-Random -Minimum 10000 -Maximum 99999)
 $RG="azrez"
