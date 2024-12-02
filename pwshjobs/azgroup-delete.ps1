1948bf117f96c4f04f58a58db469cab7d071e16d
Marian Leica <maleica@microsoft.com>
2024-12-02 16:49:51 +0200

    Update azgroup-delete.ps1

--- pwshjobs/azgroup-delete.ps1
+++ pwshjobs/azgroup-delete.ps1
@@ -1,3 +1,5 @@
+$timestamp = $(Get-Date -Format "yyyy/MM/dd-HH:mm UTCK")
+$scenario = "azgroup-delete"
 Write-Output "Deleting deployment resource group.."
 az group delete -n azrez --yes --no-wait
 Start-Sleep 5
