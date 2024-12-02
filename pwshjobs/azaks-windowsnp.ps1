1b5bb99f7962d6b440dab12eeaf92e066114f000
Marian Leica <maleica@microsoft.com>
2024-12-02 10:04:04 +0200

    Update azaks-windowsnp.ps1

--- pwshjobs/azaks-windowsnp.ps1
+++ pwshjobs/azaks-windowsnp.ps1
@@ -1,5 +1,6 @@
-
 # Setting variables
+$timestamp = $(Get-Date -Format "yyyy/MM/dd-HH:mm UTCK")
+$scenario = "azaks-windowsnp"
 $suffix=$(Get-Random -Minimum 10000 -Maximum 99999)
 $RG="azrez" # Name of resource group for the AKS cluster
 $location="uksouth" # Name of the location 
