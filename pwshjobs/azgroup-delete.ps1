dc3c1422068e109e711548163f40ddec9e1ac6a9
Marian Leica <maleica@microsoft.com>
2024-12-04 23:25:00 +0200

    Update azgroup-delete.ps1

--- pwshjobs/azgroup-delete.ps1
+++ pwshjobs/azgroup-delete.ps1
@@ -3,3 +3,6 @@ $scenario = "azgroup-delete"
 Write-Output "Deleting deployment resource group.."
 az group delete -n azrez --yes --no-wait
 Start-Sleep 5
+
+# Logging
+Write-Output "${timestamp}; {${scenario}; RG: azrez; Operation: Deleting resource group}" >> C:\azrez\azrez.log
