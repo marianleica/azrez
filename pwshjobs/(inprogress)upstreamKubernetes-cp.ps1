62903346b010463f2a8f7880dc834e987bbfdf65
Marian Leica <maleica@microsoft.com>
2024-12-07 12:18:17 +0200

    Update (inprogress)upstreamKubernetes-cp.ps1

--- pwshjobs/(inprogress)upstreamKubernetes-cp.ps1
+++ pwshjobs/(inprogress)upstreamKubernetes-cp.ps1
@@ -98,3 +98,72 @@ Write-Output "ssh ${admin}@${WORKER1IP}"
 Write-Output "ssh ${admin}@${WORKER2IP}"
 Write-Output ""
 Read-Host "Press any key to continue..."
+
+1
+
+sudo apt update && sudo apt upgrade -y
+
+2
+
+sudo swapoff -a
+
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+3
+
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+sudo apt install -y containerd.io
+
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "(!)"
+echo "Copy this information from the kubeadm init output to run on worker nodes be able to add them:"
+echo "Below is an example, your output has an unique token"
+echo "kubeadm join 192.168.0.4:6443 --token jjzu4e.xsrs0fknopxaqhhx --discovery-token-ca-cert-hash sha256:823ff397ce70aa7b3d99c2434bd07ddde27c0bf0c14d9c34eea1069ae9a44eb4"
+echo ""
+sudo kubeadm init
+
+sleep 15
+
+echo "Taking the kubeconfig file to be able to run kubectl commands:" 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+
+
+kubectl get nodes
+
+
