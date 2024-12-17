07dcc035ac3f9d7190c1c1b3f72b079c805766dc
Marian Leica <maleica@microsoft.com>
2024-12-17 22:03:38 +0200

    Update (draft)upstreamKubernetes-azureArc.ps1

--- pwshjobs/(draft)upstreamKubernetes-azureArc.ps1
+++ pwshjobs/(draft)upstreamKubernetes-azureArc.ps1
@@ -1,1 +1,10 @@
-
+# Setting variables
+$timestamp = $(Get-Date -Format "yyyy/MM/dd-HH:mm UTCK")
+$scenario = "upstreamKubernetes-infra"
+$suffix=$(Get-Random -Minimum 100 -Maximum 999)
+$suffix2=$(Get-Random -Minimum 10 -Maximum 99)
+$RG="azrez"
+$vnet="kubeadm"
+$subnet="kube"
+$admin="adm${suffix}"
+$loc="uksouth"
