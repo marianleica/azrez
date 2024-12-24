cb1dca2a5590e283463a70ec534a136f455a8e96
Marian Leica <maleica@microsoft.com>
2024-12-24 16:06:29 +0200

    Update (draft)azaks-public-lb-ilb.ps1

--- pwshjobs/(draft)azaks-public-lb-ilb.ps1
+++ pwshjobs/(draft)azaks-public-lb-ilb.ps1
@@ -45,6 +45,54 @@ Write-Output "You should be able to run kubectl commands to your cluster now"
 Write-Output ""
 Write-Output "Install kubectl locally, if needed: az aks install-cli"
 Write-Output ""
+
+# Deploy application and NodePort services
+
+kubectl create deploy tstapp1 --image=nginx:alpine --replicas 2 --port 80
+kubectl create deploy tstapp2 --image=nginx --replicas 2 --port 80
+
+kubectl expose deploy tstapp1 --type NodePort --port 80
+kubectl expose deploy tstapp2 --type NodePort --port 80
+
+# Deploy ILB for the NodePort services
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: tstapp1-ilb
+  annotations:
+    service.beta.kubernetes.io/azure-load-balancer-ipv4: 10.240.0.50
+    service.beta.kubernetes.io/azure-load-balancer-internal: "true"
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 80
+    targetPort: 80
+    nodePort: 30557
+  selector:
+    app: tstapp1
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: tstapp2-ilb
+  annotations:
+    service.beta.kubernetes.io/azure-load-balancer-ipv4: 10.240.0.51
+    service.beta.kubernetes.io/azure-load-balancer-internal: "true"
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 80
+    targetPort: 80
+    nodePort: 30558
+  selector:
+    app: tstapp2
+EOF
+
 Read-Host "Press any key to continue..."
+
+
+
 
 #################################
