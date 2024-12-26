76bf8ee4f1a5c98c414a045f0174a30913f9f3cf
Marian Leica <maleica@microsoft.com>
2024-12-26 16:51:30 +0200

    (draft)azvm-ubuntu2204docker.ps1

--- pwshjobs/(draft)azvm-ubuntu2204docker.ps1
+++ pwshjobs/(draft)azvm-ubuntu2204docker.ps1
@@ -49,41 +49,10 @@ Start-Sleep -Seconds 1
 # Logging
 Write-Output "${timestamp}; {${scenario}; RG: ${RG}; Location: ${location}; ResType: VM; ResName: ${VM}; PublicIP: ${vmip}; Admin: azrez}" >> C:\azrez\azrez.log
 
-az vm run-command create --resource-group $RG --async-execution false --run-as-user $userName --script "sudo apt-get update && sudo apt-get install ca-certificates curl && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc" --timeout-in-seconds 3600 --run-command-name "SetDockerUp" --vm-name $VM
-az vm run-command create --resource-group $RG --async-execution false --run-as-user $userName --script "echo deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin" --timeout-in-seconds 3600 --run-command-name "SetDockerUp" --vm-name $VM
-az vm run-command create --resource-group $RG --async-execution false --run-as-user $userName --script "sudo docker run hello-world" --timeout-in-seconds 3600 --run-command-name "SetDockerUp" --vm-name $VM
-
+# Run the docker install script commands inside the VM
+az vm run-command create --resource-group $RG --async-execution false --run-as-user $userName --script "sudo wget -O - https://raw.githubusercontent.com/marianleica/azrez/refs/heads/progress/pwshjobs/azvm-ubuntu2204-docker-runcommand.sh | bash" --timeout-in-seconds 3600 --run-command-name "SetDockerUp" --vm-name $VM
 
 # Look for user input to perform ssh connection right now
 $userinput = Read-Host -Prompt "Do you want to connect to ${VM} via ssh now? (y/n)"
 if ($userinput -eq "y"){az ssh vm -g $RG -n $VM --local-user $userName}
 else {Write-Output "Save the command for later: az ssh vm -g ${RG} -n ${VM} --local-user ${userName}"}
-
-# the script doesn't proceed with the config after the ssh session is started
-# we should put all lines below before the ssh prompt
-# the procedure should be with az vm invoke command to the respective vm
-
-# Add Docker's official GPG key:
-#echo "Adding the Docker's official GPG"
-#sudo apt-get update
-#sudo apt-get install ca-certificates curl
-#sudo install -m 0755 -d /etc/apt/keyrings
-#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-#echo "Adding the repository to the apt sources"
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-
-# Install the docker packages
-#echo "Now installing the docker packages"
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Run the test hello world
-echo "Let's test it with a quick hello-wolrd container"
-sudo docker run hello-world
