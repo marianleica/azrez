a974d8ba05181c5c40ddbe1ebe710411512d7ae7
Catalin Tudorache <48681880+karatheem@users.noreply.github.com>
2025-02-20 13:15:04 +0200

    Update azvm-ubuntu2204ssh.ps1
    Error catching in case C drive does not exist

--- pwshjobs/azvm-ubuntu2204ssh.ps1
+++ pwshjobs/azvm-ubuntu2204ssh.ps1
@@ -48,7 +48,11 @@ Start-Sleep -Seconds 1
 
 
 # Logging
+if (Test-Path -Path "C:\" --ErrorAction SilentlyContinue) {
 Write-Output "${timestamp}; {${scenario}; RG: ${RG}; Location: ${location}; ResType: VM; ResName: ${VM}; PublicIP: ${vmip}; Admin: azrez}" >> C:\azrez\azrez.log
+} else {
+Write-Output "C drive not found, skipping logging."
+}
 
 # Look for user input to perform ssh connection right now
 $userinput = Read-Host -Prompt "Do you want to connect to ${VM} via ssh now? (y/n)"
