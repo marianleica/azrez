881ba7ae84e0ee4df0b46530387d65328ce4193f
marianleica <marianleica@lesformidables.club>
2025-02-20 14:19:21 +0200

    typo changes in azvm code

--- pwshjobs/azvm-ubuntu2204docker.ps1
+++ pwshjobs/azvm-ubuntu2204docker.ps1
@@ -47,10 +47,10 @@ Write-Output ""
 Start-Sleep -Seconds 1
 
 # Logging
-if (Test-Path -Path "C:\" --ErrorAction SilentlyContinue) {
-Write-Output "${timestamp}; {${scenario}; RG: ${RG}; Location: ${location}; ResType: VM; ResName: ${VM}; PublicIP: ${vmip}; Admin: azrez}" >> C:\azrez\azrez.log
+if (Test-Path -Path "C:\" -ErrorAction SilentlyContinue) {
+    Write-Output "${timestamp}; {${scenario}; RG: ${RG}; Location: ${location}; ResType: VM; ResName: ${VM}; PublicIP: ${vmip}; Admin: azrez}" >> C:\azrez\azrez.log
 } else {
-Write-Output "C drive not found, skipping logging."
+    Write-Output "C drive not found, skipping logging."
 }
 
 # Run the docker install script commands inside the VM
