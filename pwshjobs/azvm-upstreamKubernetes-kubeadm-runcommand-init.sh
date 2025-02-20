350decf4e171dfd32f0b752e7bd114a129ec6bc8
Catalin Tudorache <48681880+karatheem@users.noreply.github.com>
2025-02-20 11:20:05 +0200

    Update azvm-upstreamKubernetes-kubeadm-runcommand-init.sh
    Missed a comment

--- pwshjobs/azvm-upstreamKubernetes-kubeadm-runcommand-init.sh
+++ pwshjobs/azvm-upstreamKubernetes-kubeadm-runcommand-init.sh
@@ -56,6 +56,7 @@ echo "Below is an example, your output has an unique token"
 echo "kubeadm join 192.168.0.4:6443 --token jjzu4e.xsrs0fknopxaqhhx --discovery-token-ca-cert-hash sha256:823ff397ce70aa7b3d99c2434bd07ddde27c0bf0c14d9c34eea1069ae9a44eb4"
 echo ""
 
+# Logging kubeadm output 
 sleep 1
 sudo kubeadm init > ~/kubeadminit.log
 
