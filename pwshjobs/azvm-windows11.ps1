888c3350bf8f20dee0c28c643274196ffb7b70a2
marianleica <marianleica@lesformidables.club>
2025-02-20 14:20:46 +0200

    typo changes in azvm code

--- pwshjobs/azvm-windows11.ps1
+++ pwshjobs/azvm-windows11.ps1
@@ -51,7 +51,7 @@ Write-Output ""
 Start-Sleep -Seconds 1
 
 # Logging
-if (Test-Path -Path "C:\" --ErrorAction SilentlyContinue) {
+if (Test-Path -Path "C:\" -ErrorAction SilentlyContinue) {
 Write-Output "${timestamp}; {${scenario}; RG: ${RG}; Location: ${location}; ResType: VM; ResName: ${VM}; PublicIP: ${vmip}; Admin: azrez}" >> C:\azrez\azrez.log
 } else {
 Write-Output "C drive not found, skipping logging."
