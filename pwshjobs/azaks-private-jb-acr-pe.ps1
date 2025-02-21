8f705305842bf0ed92921f9b966c6c9f8f7f4c4d
Marian Leica <marianleica@lesformidables.club>
2025-02-21 11:23:37 +0200

    Create azaks-private-jb-acr-pe.ps1

---
+++ pwshjobs/azaks-private-jb-acr-pe.ps1
@@ -0,0 +1,9 @@
+# Creates private AKS
+# Create Azure VM running ubuntu in the same VNET different subnet
+# Configure Docker inside the VM
+# Create ACR with public access disabled
+# Import example image to ACR - acr import
+# Configure private endpoint connection from ACR to AKS VNET
+# Attach ACR to AKS cluster
+# Find a way to run docker login or suggest validated commands to user
+# Suggest user on aks get-credentials to connect to the private cluster from VM
