426f4ed4392e3053cd041b1d98a9e1aec6814e98
marianleica <marianleica@lesformidables.club>
2025-02-28 18:57:13 +0200

    resolved git issue

--- pwshjobs/azvm-upstreamKubernetes-kubeadm-runcommand-init.sh
+++ pwshjobs/azvm-upstreamKubernetes-kubeadm-runcommand-init.sh
@@ -68,9 +68,8 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# Apply calico for network management 
-#kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+# Apply Weave Net for CNI 
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 
 # to select the kubeadm join command
 cat ~/kubeadminit.log | grep -i "kubeadm join" -A1 > ~/kubeadmjoin.sh
