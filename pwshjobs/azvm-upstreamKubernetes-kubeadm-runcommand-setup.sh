3bec4e3a8a8b7a2bffd110eb005d1dc80cdf7bb3
Marian Leica <marianleica@lesformidables.club>
2026-01-09 15:37:13 +0200

    Switch CNI from Calico to Weave Net
    Replaced Calico CNI installation with Weave Net.

--- pwshjobs/azvm-upstreamKubernetes-kubeadm-runcommand-setup.sh
+++ pwshjobs/azvm-upstreamKubernetes-kubeadm-runcommand-setup.sh
@@ -69,5 +69,5 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
-§
+# Apply Weave Net for CNI 
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
